c80764d97b5693cd53d93435d20dc5c0e0c86c9f
Gitesh Gawade <giteshgawade@mobilitacgitesh.trigyn.com>
2016-11-04 19:31:05 +0530

    Initial Commit

---
+++ AsyncImageDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AsyncImageDownload
+//
+//  Created by Gitesh Gawade on 04/11/16.
+//  Copyright © 2016 Gitesh Demos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AsyncImageDownload/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AsyncImageDownload
+//
+//  Created by Gitesh Gawade on 04/11/16.
+//  Copyright © 2016 Gitesh Demos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
